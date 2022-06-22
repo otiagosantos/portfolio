eb31258f487b77640beca3a416246fccb38d3a01
Tiago Santos <otiagosantos.code@gmail.com>
2022-06-22 11:04:46 -0300

    reset all files.

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  base: "https://otiagosantos.github.io/portfolio/",
+  plugins: [react()],
+  build: {
+    outDir: 'build'
+  }
 })
